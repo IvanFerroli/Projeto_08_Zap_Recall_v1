a8d8e6ea8f5cf682398fc2cb2ccc8d4e6f528c52
ivan <ivanilson.ferreira.mec@gmail.com>
2022-03-15 22:58:09 -0300

    -_-'

---
+++ src/components/StartScreen.js
@@ -0,0 +1,9 @@
+export default function StartScreen() {
+    <div className="body">
+        <div className="initial-logo-container">
+            <img src="./assets/logo.png"/>
+            <span className="initial-logo-span">ZapRecall</span>
+        </div>
+        <button className="initial-screen-button">Praticar React <img src="./assets/next.png"/></button>
+    </div>
+}
